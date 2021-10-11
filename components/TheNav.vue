fff156d4593286d2d91a2246093d0d50a900b55b
idorenyinudoh <idorenyinudoh10@outlook.com>
2021-10-11 22:34:41 +0100

    feat: create nav component and include in default layout

---
+++ components/TheNav.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+nav
+  #image-container
+    img(src="elephant.png" alt="elephant image")
+  nuxt-link(to="/") Home
+  nuxt-link(to="/elephant") Elephant
+</template>
+
+<style scoped>
+@media screen and (max-width: 1023px) {
+  nav {
+    padding-left: calc(15px + 1vw);
+    grid-template-columns: repeat(3, max-content);
+    column-gap: 20px;
+  }
+  #image-container {
+    width: calc(70px + 2vw);
+    margin: calc(10px + 1vw) 0;
+    padding-top: 100%;
+  }
+  a {
+    padding: calc(31px + 2vw) calc(10px + 1vw);
+  }
+}
+@media screen and (min-width: 1024px) {
+  nav {
+    grid-template-rows: auto max-content;
+    width: calc(250px + 1vw);
+    height: 100vh;
+  }
+  #image-container {
+    width: calc(150px + 1vw);
+    margin: 0 50px;
+    padding-top: calc(100% - 100px);
+  }
+  a {
+    padding: 15px 30px;
+    align-self: baseline;
+  }
+}
+nav {
+  display: grid;
+  align-items: center;
+  background-color: #DDF;
+}
+#image-container {
+  position: relative;
+  background-color: #FFF;
+  border-radius: 50%;
+}
+img {
+  position: absolute;
+  width: 67%;
+  top: 19%;
+  left: 16.5%;
+}
+a {
+  text-decoration: none;
+  color: #000;
+}
+.nuxt-link-active {
+  background-color: #B9B9FF;
+  color: #FFF;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,29 @@
 <template lang="pug">
-  div
+  #container
+    the-nav
     nuxt
 </template>
+
+<script>
+import TheNav from "../components/TheNav.vue";
+
+export default {
+  components: {
+    TheNav,
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Overpass&display=swap');
+body {
+  margin: 0;
+  font-family: 'Overpass', sans-serif;
+  font-size: 18px;
+  letter-spacing: -0.5px;
+  line-height: 28px;
+}
+#container {
+  background-color: #F5F8FF;
+}
+</style>
